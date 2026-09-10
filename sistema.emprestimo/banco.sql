4f6a1752fa1185e5f0a4f4238582357b8da789ed
Lais Vargas <lais_a_vargas@estudante.sesisenai.org.br>
2026-09-10 16:25:57 -0300

    final1

--- sistema.emprestimo/banco.sql
+++ sistema.emprestimo/banco.sql
@@ -10,6 +10,4 @@ INSERT INTO equipamentos (nome, categoria, condicao_uso, disponivel)
 VALUES
 ('Notebook Lenovo', 'Notebook', 'Bom', TRUE),
 ('Notebook Dell', 'Notebook', 'Excelente', TRUE),
-('Projetor Epson', 'Projetor', 'Bom', FALSE),
-('Kit Arduino', 'Robótica', 'Excelente', TRUE),
-('Kit LEGO Mindstorms', 'Robótica', 'Regular', TRUE);
+('Projetor', 'Projetor', 'Bom', FALSE),
